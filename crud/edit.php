6952695b5a6f58086a8179f159fb2fd1a6165b8d
M. Hikmal Maulana <114215531+mhmcool@users.noreply.github.com>
2022-10-05 20:08:42 +0700

    Add files via upload

---
+++ crud/edit.php
@@ -0,0 +1,72 @@
+<?php 
+include 'koneksi.php';
+if (isset($_GET['id'])) {
+	if ($_GET['id'] != "") {
+		
+		$id = $_GET['id'];
+
+		$query = mysqli_query($koneksi,"SELECT * FROM siswa WHERE id_siswa='$id'");
+		$row = mysqli_fetch_array($query);
+
+	}else{
+		header("location:index.php");
+	}
+}else{
+	header("location:index.php");
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Bootstrap CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+	
+	<title>Cara Membuat CRUD Upload File Menggunakan PHP Dan MySQLi - belajarwithib</title>
+</head>
+<body>
+	<div class="container mt-5 ">
+		<center class="mb-5" ><h2>Cara Membuat CRUD Upload File Menggunakan PHP Dan MySQLi - belajarwithib</h2></center>
+		<hr>
+		<form action="edit_act.php" method="post" enctype="multipart/form-data">
+			<div class="mb-3">
+				<label class="form-label">Nama Lengkap Siswa</label>
+				<input type="text" name="nama_lengkap" class="form-control" value="<?php echo $row['siswa_nama']; ?>" >
+				<input type="hidden" name="id" class="form-control" value="<?php echo $row['id_siswa']; ?>" >
+			</div>
+			<div class="mb-3">
+				<label class="form-label">Email</label>
+				<input type="text" name="email" class="form-control" value="<?php echo $row['siswa_email']; ?>">
+			</div>
+			<div class="mb-3">
+				<label class="form-label">Jurusan</label>
+				<input type="text" name="jurusan" class="form-control" value="<?php echo $row['siswa_jurusan']; ?>">
+			</div>
+			<div class="mb-3">
+				<label class="form-label">Fakultas</label>
+				<input type="text" name="fakultas" class="form-control" value="<?php echo $row['siswa_fakultas']; ?>">
+			</div>
+			<div class="mb-3">
+				<label class="form-label">Pas Foto</label>
+				<input type="file" name="pas_foto" class="form-control">
+				<br>
+				<?php 
+				if ($row['siswa_foto'] == "") { ?>
+					<img src="https://via.placeholder.com/500x500.png?text=PAS+FOTO+SISWA" style="width:100px;height:100px;">
+				<?php }else{ ?>
+					<img src="foto/<?php echo $row['siswa_foto']; ?>" style="width:100px;height:100px;">
+				<?php } ?>
+			</div>
+			<div class="mb-3">
+				<button class="btn btn-success" type="submit">Submit</button>
+				<a href="index.php" class="btn btn-danger">Kembali</a>
+			</div>
+		</form>
+		
+	</div>
+</body>
+</html>
